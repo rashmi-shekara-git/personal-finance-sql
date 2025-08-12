75b648db0ac9daa0e5fd874f032e4e6f87a05b93
rashmi-shekara-git <rashmi.vr3@gmail.com>
2025-08-12 16:09:16 -0400

    Update analysis_queries.sql

--- analysis_queries.sql
+++ analysis_queries.sql
@@ -18,3 +18,31 @@ SELECT
 FROM transactions
 GROUP BY month
 ORDER BY month;
+
+-- Average spending per user
+SELECT u.name, ROUND(AVG(t.amount), 2) AS avg_spent
+FROM users u
+JOIN transactions t ON u.user_id = t.user_id
+GROUP BY u.name;
+
+-- User spending trends over time (monthly)
+SELECT 
+    u.name, 
+    EXTRACT(YEAR FROM txn_date) AS year,
+    EXTRACT(MONTH FROM txn_date) AS month,
+    ROUND(SUM(t.amount), 2) AS monthly_spend
+FROM users u
+JOIN transactions t ON u.user_id = t.user_id
+GROUP BY u.name, year, month
+ORDER BY u.name, year, month;
+
+-- Categories where user spends the most
+SELECT 
+    u.name, 
+    c.category_name, 
+    ROUND(SUM(t.amount), 2) AS total_spent
+FROM users u
+JOIN transactions t ON u.user_id = t.user_id
+JOIN categories c ON t.category_id = c.category_id
+GROUP BY u.name, c.category_name
+ORDER BY u.name, total_spent DESC;
